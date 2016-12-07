487e4e154e3e720333929eee96cccd1770183ae7
Serkan OZAL <serkanozal86@hotmail.com>
2016-12-07 22:15:35 +0300

    Improvements for client-server architecture

--- src/main/java/tr/com/serkanozal/dynacast/DynaCast.java
+++ src/main/java/tr/com/serkanozal/dynacast/DynaCast.java
@@ -23,11 +23,11 @@ import tr.com.serkanozal.dynacast.storage.DynaCastStorage;
 import tr.com.serkanozal.dynacast.storage.DynaCastStorageType;
 import tr.com.serkanozal.dynacast.storage.impl.DynaCastStorageManager;
 
-public class DynaCast2 {
+public class DynaCast {
 
     public static final String VERSION = "1.0.1-SNAPSHOT";
     
-    private static final Logger LOGGER = Logger.getLogger(DynaCast2.class);
+    private static final Logger LOGGER = Logger.getLogger(DynaCast.class);
     
     public static <K, V> DynaCastStorage<K, V> getStorage(String storageName) {
         return DynaCastStorageManager.getStorage(storageName);
--- src/main/java/tr/com/serkanozal/dynacast/DynaCastDemo.java
+++ src/main/java/tr/com/serkanozal/dynacast/DynaCastDemo.java
@@ -7,7 +7,7 @@ public class DynaCastDemo {
 
     public static void main(String[] args) {
         DynaCastStorage<Integer, String> storage = 
-                DynaCast2.getOrCreateStorage("MyStorage", DynaCastStorageType.TIERED);
+                DynaCast.getOrCreateStorage("MyStorage", DynaCastStorageType.TIERED);
         
         ///////////////////////////////////////////////////////////////////////
         
